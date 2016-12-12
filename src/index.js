fbff5ab5752f802414974db3dc7a438179b91b40
Chris Garrett <cgarrett@mackaylaw.ca>
2016-12-12 14:57:20 -0700

    es7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 
-const hello = (name) => console.log("Hello " + name);
-hello("Chris");
+const hello = (name) => new Promise((resolve, reject) => {
+  resolve(`Hello ${name}`)
+});
+
+hello('Chris')
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((err) => {
+    console.error(`There was an error! ${err}`);
+  });
+
+const main = async () => {
+  const res = await hello('Christopher');
+  console.log('res', res);
+};
+
+main();
